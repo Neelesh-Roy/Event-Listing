095f9e4518c0f64a4821af1cbec8a6cabaa1bacb
Neelesh <Neeleshroy@techie.com>
2015-07-07 15:21:59 +0530

    configured routes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,2 @@
-
-var eventApp = angular.module('eventApp', ['ngRoute', 'eventControllers']);
-
-var eventController = angular.module('eventControllers' , []);
-
-eventApp.config(['$routeProvider', function($routeProvider){
-  $routeProvider.
-    when('/login',{
-      templateUrl : 'views/login.html'
-    }).
-      when('/register',{
-        templateUrl : 'views/register.html'
-    }).
-      when('/events',{
-        templateUrl : 'views/events.html'
-    });
-}]);
+// jshint devel:true
+console.log('\'Allo \'Allo!');
